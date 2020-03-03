36a354e04f0786dc4ef5a57ef784fddbe820cac1
anicholas4747 <an1720@nyu.edu>
2020-03-03 12:11:36 -0500

    mouse tracker working

--- src/game.js
+++ src/game.js
@@ -1,20 +1,13 @@
-import './styles/main.scss';
+import './styles/main.css';
 import './scripts/play';
+import MouseTracker from './scripts/flex/mouse_tracking';
 
 window.addEventListener("DOMContentLoaded", () => {
-  //play.js
-  // const canvas = document.createElement("canvas");
-  // canvas.id = "canvas";
   const canvas = document.getElementById("canvas");
-  canvas.style.backgroundColor = "white";
-  // const screen = document.getElementById("screen");
-  // screen.appendChild(canvas);
+  canvas.style.backgroundColor = "#DDD";
   let c = canvas.getContext("2d");
 
-  c.beginPath();
-  // c.lineWidth = 10;
-  c.arc(300, 300, 30, 0, Math.PI * 2, false);
-  // c.strokeStyle = "#065675";
-  c.strokeStyle = "blue";
-  c.stroke();
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  
 });
---
+++ src/scripts/flex/mouse_tracking.js
@@ -0,0 +1,116 @@
+const canvas = document.getElementById("canvas");
+const c = canvas.getContext("2d");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const mouse = {
+  x: 0,
+  y: 0
+};
+
+let clicked = false;
+
+// Event Listeners
+addEventListener('mousemove', (event) => {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
+
+addEventListener('mousedown', (event) => {
+  clicked = true;
+});
+
+addEventListener('mouseup', (event) => {
+  clicked = false;
+});
+
+addEventListener('resize', () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+
+  init();
+});
+
+
+class MouseTracker {
+  constructor(x, y, radius, color){
+    this.origX = x;
+    this.origY = y;
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.radians = Math.random() * Math.PI * 2;
+    this.velocity = this.randInt(1, 4) / this.randInt(100,150);
+    this.distanceFromCenter = this.randInt(55, 177);
+    this.lastMouse = {
+      x: this.origX,
+      y: this.origY,
+    };
+  }
+
+  randInt(low,high){
+    return Math.floor(Math.random() * (high - low + 1) + low);
+  }
+
+  update(){
+
+    const lastPoint = {
+      x: this.x,
+      y: this.y
+    };
+    this.radians += this.velocity;
+
+    this.lastMouse.x += (mouse.x - this.lastMouse.x) * 0.05;
+    this.lastMouse.y += (mouse.y - this.lastMouse.y) * 0.05;
+
+    if(clicked) {
+      this.x = this.lastMouse.x;
+      this.y = this.lastMouse.y;
+    } else {
+      this.x = this.lastMouse.x + Math.cos(this.radians) * this.distanceFromCenter;
+      this.y = this.lastMouse.y + Math.sin(this.radians) * this.distanceFromCenter;
+    }
+    
+    this.draw(lastPoint);
+  }
+
+  draw(lastPoint){
+    c.beginPath();
+    c.strokeStyle = this.color;
+    c.lineWidth = this.radius;
+    c.moveTo(lastPoint.x, lastPoint.y);
+    c.lineTo(this.x, this.y);
+    c.stroke();
+    c.closePath();
+  }
+}
+
+const colors = ["#0068A1", "#DB0700", "#FFC513", "#EFFBFF", "#579CC2"];
+
+let liddleBuddies = [];
+let init = () => {
+  liddleBuddies = [];
+  for(let i = 0; i < 200; i++){
+    let radius = Math.floor(Math.random() * (7 - 3 + 1) + 3);
+    let color = colors[Math.floor(Math.random() * 5)];
+    liddleBuddies.push(new MouseTracker(1000, 500, radius, color));
+  }
+  console.log(liddleBuddies);
+};
+
+// Animation Loop
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillStyle = "rgba(255,255,255,0.05)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  liddleBuddies.forEach(bud => {
+   bud.update();
+  });
+}
+
+init();
+animate();
+
+export default MouseTracker;
