4d458f48807c73f2680dc68a7510305cd3e44707
anicholas4747 <an1720@nyu.edu>
2020-03-02 21:22:40 -0500

    Render screen

--- src/game.js
+++ src/game.js
@@ -1,1 +1,20 @@
 import './styles/main.scss';
+import './scripts/play';
+
+window.addEventListener("DOMContentLoaded", () => {
+  //play.js
+  // const canvas = document.createElement("canvas");
+  // canvas.id = "canvas";
+  const canvas = document.getElementById("canvas");
+  canvas.style.backgroundColor = "white";
+  // const screen = document.getElementById("screen");
+  // screen.appendChild(canvas);
+  let c = canvas.getContext("2d");
+
+  c.beginPath();
+  // c.lineWidth = 10;
+  c.arc(300, 300, 30, 0, Math.PI * 2, false);
+  // c.strokeStyle = "#065675";
+  c.strokeStyle = "blue";
+  c.stroke();
+});
